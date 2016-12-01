94a3682000e9496e184ec032dde42ac692dec382
Sam Noedel <sam.noedel@changepoint.com>
2016-12-01 07:19:56 -0800

    Add PC-1 table

--- src/main.c
+++ src/main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include "key.h"
 
 int main(int argc, char **argv)
 {
-	printf("Called with %d arguments", argc);
+	for (short i = 0; i < 56; i++) {
+		printf("%d ", PC_1[i]);
+		if ((i + 1) % 7 == 0) {
+			printf("\n");
+		}
+	}
+	printf("\n");
 	return 0;
 }
